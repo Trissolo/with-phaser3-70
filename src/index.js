c87390646325c6123cbbb3ad88c652ac3fa0706f
Trissolo <00tristano00@gmail.com>
2023-11-15 05:38:50 +0100

    Just import external stuff, them start the game

--- src/index.js
+++ src/index.js
@@ -1,52 +1,22 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
+
+// import logoImg from './assets/logo.png';
 
 import TemplateScene from './Scenes/TemplateScene.mjs';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
-
-    preload ()
-    {
-        //  This is an example of a bundled image:
-        this.load.image('logo', logoImg);
-
-        //  This is an example of loading a static image from the public folder:
-        this.load.image('background', 'assets/bg.jpg');
-
-        this.load.image('bitsy', 'assets/nonpermanent/bitsy-6x8.png')
-    }
-      
-    create ()
-    {
-        this.add.image(8, 8, 'bitsy').setOrigin(0);
-
-        this.add.image(11, 12, 'logo').setOrigin(0);
-      
-        this.scene.launch('TemplateScene');
-
-    }
-}
-
-const config = {
-    // type: Phaser.AUTO,
+/*  window.game =  */ new Phaser.Game({
+    // type: Phaser.WEBGL,
     pixelArt: true,
     backgroundColor: '#320822',
     disableContextMenu: true,
     parent: 'phaser-example',
     scale:
     {
+        // autoCenter: Phaser.Scale.CENTER_BOTH,
         mode: Phaser.Scale.NONE,
-        //autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 256,
         height: 200,
         zoom: 3
     },
-    scene: [MyGame, TemplateScene]
-};
-
-const game = new Phaser.Game(config);
+    scene: TemplateScene
+});
