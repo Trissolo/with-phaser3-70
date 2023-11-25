8d2f450f3a115cd532591a5f57f00ae592f55cac
Trissolo <00tristano00@gmail.com>
2023-11-25 11:31:41 +0100

    Remove ternary operation

--- src/scenes/snippets/PipelineReverseMode.mjs
+++ src/scenes/snippets/PipelineReverseMode.mjs
@@ -11,8 +11,8 @@ export default class PipelineReverseMode extends Phaser.Renderer.WebGL.Pipelines
             precision mediump float;
 
             uniform sampler2D uMainSampler;
-            uniform vec4 colorA;
-            uniform vec4 colorB;
+            uniform vec3 colorA;
+            uniform vec3 colorB;
 
             varying vec2 outTexCoord;
             
@@ -22,7 +22,9 @@ export default class PipelineReverseMode extends Phaser.Renderer.WebGL.Pipelines
             {
                 vec4 texture = texture2D(uMainSampler, outTexCoord);
 
-                texture.rgba =  texture.a > 0.99? colorB: colorA;
+                //texture.rgba = texture.a == 1.0? vec4(colorB, 1.0): vec4(colorA, 1.0);
+
+                texture.rgba = vec4(colorB * texture.a + colorA * abs(1.0 - texture.a), 1.0);
 
                 gl_FragColor = texture;
                                
@@ -40,14 +42,14 @@ export default class PipelineReverseMode extends Phaser.Renderer.WebGL.Pipelines
         // this.setBGCol();
     }
 
-    setCharCol(r = 0.2, g = 0.2, b = 1, a)
-    {
-        this.set4f('colorB', r, g, b, 1);
-    }
+    // setCharCol(r = 0.2, g = 0.2, b = 1, a)
+    // {
+    //     this.set4f('colorB', r, g, b, 1);
+    // }
 
-    setBGCol(r = 0.5, g = 0.6, b = 1, a)
-    {
-        // this.set4f('colorA', r, g, b, 1);
-        // this.customPipeline.set4f('color_b', 0.2, 0.2, 1, 1);
-    }
+    // setBGCol(r = 0.5, g = 0.6, b = 1, a)
+    // {
+    //     // this.set4f('colorA', r, g, b, 1);
+    //     // this.customPipeline.set4f('color_b', 0.2, 0.2, 1, 1);
+    // }
 }
