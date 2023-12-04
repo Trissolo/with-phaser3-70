6a9b390dd592e2f861fb5035d0b5cde788112f37
Trissolo <00tristano00@gmail.com>
2023-12-04 07:45:15 +0100

    Add 'debugPolygonContains'

--- src/scenes/snippets/GeneratorFuncTest.mjs
+++ src/scenes/snippets/GeneratorFuncTest.mjs
@@ -56,12 +56,39 @@ export default class GeneratorFuncTestDebug extends Phaser.Scene
 
         graphics.strokePoints(polygon.points, true);
 
+        graphics.lineStyle(2, 0x88aa88);
+
         
         this.genarator = this.debugStepByStep(graphics, polygon);
         
         // advance by pressing Z
         this.input.keyboard.on("keydown-Z", () => this.genarator.next())
 
+    }
+
+    *debugPolygonContains(polygon, x, y, graphics)
+    {
+        let inside = false;
+        const line = new Phaser.Geom.Line();
+        const point = new Phaser.Math.Vector2(x, y);
+
+        for (var i = -1, j = polygon.points.length - 1; ++i < polygon.points.length; j = i)
+        {
+            // var ix = polygon.points[i].x;
+            // var iy = polygon.points[i].y;
+            const {x:ix, y:iy} = polygon.points[i];
+
+            // var jx = polygon.points[j].x;
+            // var jy = polygon.points[j].y;
+            const {x:jx, y:jy} = polygon.points[j];
+
+            if (((iy <= y && y < jy) || (jy <= y && y < iy)) && (x < (jx - ix) * (y - iy) / (jy - iy) + ix))
+            {
+                inside = !inside;
+            }
+        }
+
+        return inside;
     }
 
     *debugStepByStep(graphics, polygon)
